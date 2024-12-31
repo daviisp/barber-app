48cc921651cbd3a64ee504a996ad9c0a18685a0a
Davi Silva <davi.pimenta8@gmail.com>
2024-12-31 19:30:35 -0300

    feat: add component to show a user

---
+++ src/app/(home)/_components/welcome.user.tsx
@@ -0,0 +1,10 @@
+export const WelcomeUser = () => {
+  return (
+    <section className="space-y-1">
+      <p className="text-lg">
+        Olá, <span className="font-bold">Miguel!</span>
+      </p>
+      <p className="text-sm">Sexta, 2 de Fevereiro</p>
+    </section>
+  );
+};
