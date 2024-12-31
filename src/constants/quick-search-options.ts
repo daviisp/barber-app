cd9396451d6f90a22fd701ec91adb8bdef1c8737
Davi Silva <davi.pimenta8@gmail.com>
2024-12-31 19:31:42 -0300

    feat: add options services of barbershops

---
+++ src/constants/quick-search-options.ts
@@ -0,0 +1,31 @@
+interface QuickSearchOption {
+  imageUrl: string;
+  title: string;
+}
+
+export const quickSearchOptions: QuickSearchOption[] = [
+  {
+    imageUrl: "/hair.svg",
+    title: "Cabelo",
+  },
+  {
+    imageUrl: "/mustache.svg",
+    title: "Barba",
+  },
+  {
+    imageUrl: "/razor-double.svg",
+    title: "Acabamento",
+  },
+  {
+    imageUrl: "/massage.svg",
+    title: "Massagem",
+  },
+  {
+    imageUrl: "/eyebrow.svg",
+    title: "Sobrancelha",
+  },
+  {
+    imageUrl: "/hydration.svg",
+    title: "Hidratação",
+  },
+];
