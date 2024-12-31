e2deb32b686fdb237718288d65f3e51a5f310de4
Davi Silva <davi.pimenta8@gmail.com>
2024-12-31 20:00:26 -0300

    fix: adjust padding in card

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -57,7 +57,7 @@ const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("px-6 pt-0 pb-0", className)} {...props} />
+  <div ref={ref} className={cn("pl-6 pt-0 pb-0", className)} {...props} />
 ));
 CardContent.displayName = "CardContent";
 
